70017a981a903f4ae95b436edff08d78bbfb6473
Fidel Sosa <fsosa@eharmony.com>
2012-10-13 19:05:38 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trucklr::Application.config.secret_token = 'fb4033d32cbac61b5e515ddad9f7b7d1e0168eb538e8fe3bafafdf1ac51df5783690d9b9543d7ecdd5c4f687029c14874f984dca00e07a4049690dc0fa7746d7'
